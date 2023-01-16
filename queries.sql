51ed635023d6f2a498cecfde1574c1e50d5c6c25
manq2010 <manq2010@gmail.com>
2023-01-16 17:52:18 +0200

    Make required changes

--- queries.sql
+++ queries.sql
@@ -5,7 +5,7 @@ SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-3
 SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR 
 name = 'Pikachu';
-SELECT name, escape_attempts FROM animals WHERE weight_kg <= 10.50;
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.50;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.40 AND 17.30;
