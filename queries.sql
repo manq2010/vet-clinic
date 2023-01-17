f91617439592bece68ade8c9fb66696df997c533
manq2010 <manq2010@gmail.com>
2023-01-17 17:15:55 +0200

    Update results to queries

--- queries.sql
+++ queries.sql
@@ -41,4 +41,6 @@ SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
 SELECT AVG(weight_kg) FROM animals;
 SELECT SUM(escape_attempts) FROM animals WHERE neutered = true;
 SELECT neutered, COUNT(*) FROM animals WHERE escape_attempts > 1 GROUP BY neutered;
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) as avg_escape FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
 
