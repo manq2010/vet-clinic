dbbead0b8bf609b729b72c0c495fc83280e38180
manq2010 <manq2010@gmail.com>
2023-01-25 10:39:18 +0200

    Add join table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -27,3 +27,22 @@ CREATE TABLE invoices (
     medical_history_id INT NOT NULL REFERENCES medical_histories(id)
 );
 CREATE INDEX invoices_medical_history_id_idx ON invoices (medical_history_id);
+
+CREATE TABLE invoice_items (
+    id SERIAL PRIMARY KEY,
+    unit_price DECIMAL(4,2) NOT NULL,
+    quantity INT NOT NULL,
+    total_price DECIMAL(4,2) NOT NULL,
+    invoice_id INT NOT NULL REFERENCES invoices(id),
+    treatment_id INT NOT NULL REFERENCES treatments(id)
+);
+CREATE INDEX invoice_items_invoice_id_idx ON invoice_items (invoice_id);
+CREATE INDEX invoice_items_treatment_id_idx ON invoice_items (treatment_id);
+
+CREATE TABLE medical_history_treatment (
+    id SERIAL PRIMARY KEY,
+    medical_history_id INT NOT NULL REFERENCES medical_histories(id),
+    treatment_id INT NOT NULL REFERENCES treatments(id)
+);
+CREATE INDEX medical_history_treatment_medical_history_id_idx ON medical_history_treatment (medical_history_id);
+CREATE INDEX medical_history_treatment_treatment_id_idx ON medical_history_treatment (treatment_id);
